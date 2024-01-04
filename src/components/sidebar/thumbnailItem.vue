b1014399fd52c404ea16f5770b488b11d09f35f4
이지현 <jhyi@inswave.com>
2024-01-04 21:26:21 +0900

    썸네일 표출되도록 수정

--- src/components/sidebar/thumbnailItem.vue
+++ src/components/sidebar/thumbnailItem.vue
@@ -1,6 +1,6 @@
 <template>
   <ul class="md:flex-col md:min-w-full flex flex-col list-none">
-    <li class="items-center" v-for="(item, idx) of templateImages" :key="idx">
+    <li class="items-center" v-for="(item, idx) in templateImages" :key="idx">
       <div class="border p-2 thumbnail">
         <img class="thumbnail-img" :src="item" />
       </div>
@@ -8,7 +8,7 @@
   </ul>
 </template>
 <script setup lang="ts">
-import {ref, watch, computed} from 'vue'
+import {ref, watch, computed, onMounted} from 'vue'
 import { useTemplateStore, type ItemplateImg } from '@/stores/document'
 import htmlToCanvas from 'html2canvas'
 
@@ -19,60 +19,48 @@ import htmlToCanvas from 'html2canvas'
 const docStore = useTemplateStore()
 const selectTemplateId = docStore.getSelectTemplateId()
 const curTemplate = docStore.getTemplate(selectTemplateId)
-// const templateImages: ItemplateImg[] = new Array<ItemplateImg>()
 const templateImages = ref(new Array<string>())
-// const thumbnailImages = ref(templateImages)
 
-watch(templateImages, (newValue, oldValue) => {
-  console.log('here')
-  console.log(newValue)
-  templateImages.value = newValue
-})
+onMounted(async ()=>{
+  let domElems = new DOMParser().parseFromString(curTemplate.htmlStr, "text/html")
 
+  //here
+  let contentDomElem = Array.from(domElems.querySelector('.ck-content')?.children!)
+  let container = document.createElement('div')
 
-let domElems = new DOMParser().parseFromString(curTemplate.htmlStr, "text/html")
+  for (let i = 0; i < contentDomElem!.length; i++) {
+    if (contentDomElem[i].className.indexOf('page-break') > -1) {
+      // TODO 썸네일 만들기
+      document.body.appendChild(container);
 
+      let canvas = await htmlToCanvas(container)
 
-debugger
-//here
-let contentDomElem = Array.from(domElems.querySelector('.ck-content')?.children!)
-let container = document.createElement('div')
+      let image = canvas.toDataURL()
+      // console.log(image)
+      templateImages.value.push(image)
+      document.body.removeChild(container)
+      container.remove()
+      container = document.createElement('div')
 
-for(let i=0; i<contentDomElem!.length; i++) {
-  if(contentDomElem[i].className.indexOf('page-break') > -1) {
-    // TODO 썸네일 만들기
-    document.body.appendChild(container);
+    } else {
+      container.appendChild(contentDomElem[i])
+    }
+  }
 
+  if (container.innerHTML) {
+    document.body.appendChild(container)
     let canvas = await htmlToCanvas(container)
 
     let image = canvas.toDataURL()
-    // console.log(image)
+    console.log(image)
+    debugger
     templateImages.value.push(image)
-    document.body.removeChild(container)
+    // document.body.removeChild(container)
     container.remove()
-    container = document.createElement('div')
 
-  } else {
-    container.appendChild(contentDomElem[i])
   }
-}
 
-if(container.innerHTML) {
-  document.body.appendChild(container)
-  let canvas = await htmlToCanvas(container)
-
-  let image = canvas.toDataURL()
-  console.log(image)
-  debugger
-  templateImages.value.push(image)
-  // document.body.removeChild(container)
-  container.remove()
-
-}
-
-
-// const templateImages: ItemplateImg[] = new Array<ItemplateImg>()
-// templateImages.push(curTemplate)
+})
 </script>
 
 <style scoped>
